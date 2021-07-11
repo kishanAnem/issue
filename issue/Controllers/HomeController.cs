dea3560a1624448b0b7970f9e48122222edcb429
kishanAnem <kishan3034@gmail.com>
2021-07-11 19:49:49 +0530

    first commit

---
+++ issue/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using issue.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace issue.Controllers
+{
+    public class UserDto
+    {
+        public List<string> Location { get; set; } = new();
+    }
+
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index(UserDto model)
+        {
+            var userDto = new UserDto();
+            await TryUpdateModelAsync(userDto);
+            return View(userDto);
+        }
+
+
+    }
+}
---
+++ issue/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace issue.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
